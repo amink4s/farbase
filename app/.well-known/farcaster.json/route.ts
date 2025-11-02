3e61340b943c84856bcaa373b82b0b1e91df8d93
Mikadoe <170118553+amink4s@users.noreply.github.com>
2025-11-02 16:21:40 +0000

    account association

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,5 +2,15 @@ import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  const body = {
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjQ3NzEyNiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxOGE5YjU4QjkyOWRCRDBEOTEzMkFENEZlYzBkRkRCNzkyNDUyYkQifQ",
+      payload: "eyJkb21haW4iOiJmYXJiYXNlLXBoaS52ZXJjZWwuYXBwIn0",
+      signature:
+        "4kyIU8gLozkwkRaKZMoPg0siwFqXB8uIYUqdyvg6be1r0WxPKNsJtgXyrKTMDf/fUdyvv1P1aUFssI8Pp5xWzRs="
+    }
+  };
+
+  return Response.json(body);
 }
